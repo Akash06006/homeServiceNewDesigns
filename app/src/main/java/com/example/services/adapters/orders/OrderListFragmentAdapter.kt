0f43bd9e5650cea238b845dc0b25978c884d9240
ashishsecrets <ashishsecrets@users.noreply.github.com>
2020-10-13 10:45:17 +0530

    history screen

--- app/src/main/java/com/example/services/adapters/orders/OrderListFragmentAdapter.kt
+++ app/src/main/java/com/example/services/adapters/orders/OrderListFragmentAdapter.kt
@@ -1,8 +1,5 @@
 package com.uniongoods.adapters
 
-import android.content.Intent
-import android.content.res.ColorStateList
-import android.graphics.Color
 import android.os.Build
 import android.view.LayoutInflater
 import android.view.View
@@ -16,13 +13,9 @@ import com.example.services.R
 import com.example.services.constants.GlobalConstants
 import com.example.services.databinding.OrderItemBinding
 import com.example.services.model.orders.OrdersListResponse
-import com.example.services.socket.DriverTrackingActivity
 import com.example.services.utils.BaseActivity
 import com.example.services.utils.Utils
-import com.example.services.views.orders.OrdersListActivity
 import com.example.services.views.orders.OrdersListFragment
-import com.google.gson.JsonObject
-import kotlinx.android.synthetic.main.order_item.*
 import java.util.*
 import kotlin.collections.ArrayList
 
@@ -57,84 +50,9 @@ class OrderListFragmentAdapter(
     @RequiresApi(Build.VERSION_CODES.LOLLIPOP)
     override fun onBindViewHolder(@NonNull holder: ViewHolder, position: Int) {
         viewHolder = holder
-        holder.binding!!.tvOrderOn.text = Utils(mContext).getDate(
-            "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'",
-            addressList[position].createdAt,
-            "HH:mm yyyy-MM-dd"
-        )
-        holder.binding!!.tvServiceOn.text = Utils(mContext).getDate(
-            "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'",
-            addressList[position].serviceDateTime,
-            "HH:mm yyyy-MM-dd"
-        )
 
-        if (orderListActivity != null && addressList[position].trackStatus!!.equals("1")) {
-            holder.binding!!.tvTrack.visibility = View.VISIBLE
-        } else {
-            holder.binding!!.tvTrack.visibility = View.GONE
-        }
-        holder.binding!!.tvTotal.setText(GlobalConstants.Currency + " " + addressList[position].totalOrderPrice)
-////0-Pending/Not Confirmed, 1-> Confirmed , 2->Cancelled , 3->Processing,4//cancelled by company, 5->Completed
-        if (addressList[position].cancellable.equals("true")) {
-            holder.binding!!.tvCancel.setText("Cancel Order"/*addressList[position].progressStatus*/)
-            holder.binding!!.tvCancel.isEnabled = true
-        } else {
 
-            if (addressList[position].progressStatus.equals("0")) {
-                holder.binding!!.tvCancel.setText("Pending"/*addressList[position].progressStatus*/)
-                holder.binding!!.tvCancel.isEnabled = true
-                holder.binding!!.tvCancel.setBackgroundTintList(
-                    mContext.getResources().getColorStateList(
-                        R.color.colorStatus
-                    )
-                )
-
-            } else if (addressList[position].progressStatus.equals("1")) {
-                holder.binding!!.tvCancel.setText("Confirmed"/*addressList[position].progressStatus*/)
-                holder.binding!!.tvCancel.isEnabled = true
-                holder.binding!!.tvCancel.setBackgroundTintList(
-                    mContext.getResources().getColorStateList(
-                        R.color.colorStatus
-                    )
-                )
-
-            } else if (addressList[position].progressStatus.equals("2")) {
-                holder.binding!!.tvCancel.setText("Cancelled"/*addressList[position].progressStatus*/)
-                holder.binding!!.tvCancel.isEnabled = false
-            } else if (addressList[position].progressStatus.equals("3")) {
-                holder.binding!!.tvCancel.isEnabled = true
-                holder.binding!!.tvCancel.setBackgroundTintList(
-                    mContext.getResources().getColorStateList(
-                        R.color.colorStatus
-                    )
-                )
-
-                holder.binding!!.tvCancel.setText("Processing"/*addressList[position].progressStatus*/)
-            } else if (addressList[position].progressStatus.equals("4")) {
-                holder.binding!!.tvCancel.isEnabled = false
-                holder.binding!!.tvCancel.setText("Cancelled by company"/*addressList[position].progressStatus*/)
-            } else if (addressList[position].progressStatus.equals("5")) {
-                holder.binding!!.tvCancel.isEnabled = false
-                holder.binding!!.tvCancel.setText("Completed"/*addressList[position].progressStatus*/)
-                holder.binding!!.tvCancel.setBackgroundTintList(
-                    mContext.getResources().getColorStateList(
-                        R.color.colorSuccess
-                    )
-                )
-
-            }
-        }
-
-        holder.binding!!.tvCancel.setOnClickListener {
-            if (addressList[position].cancellable.equals("true")) {
-                if (orderListActivity != null)
-                    orderListActivity.cancelOrder(position)
-            } else {
-                orderListActivity!!.completeOrder(position)
-            }
-        }
-
-        holder.binding!!.tvTrack.setOnClickListener {
+        /*holder.binding!!.tvTrack.setOnClickListener {
             val mJsonObjectStartJob = JsonObject()
             mJsonObjectStartJob.addProperty(
                 "orderId", addressList[position].id
@@ -155,7 +73,7 @@ class OrderListFragmentAdapter(
             intent.putExtra("data", mJsonObjectStartJob.toString())
             mContext.startActivity(intent)
 
-        }
+        }*/
 
 
         /* if (orderListActivity == null) {
@@ -164,7 +82,12 @@ class OrderListFragmentAdapter(
              holder.binding!!.tvCancel.visibility = View.VISIBLE
          }*/
         val orderListAdapter =
-            OrderServicesListAdapter(mContext, addressList[position].suborders, mContext)
+            OrderServicesListAdapter(
+                mContext,
+                addressList[position].suborders,
+                addressList,
+                mContext
+            )
         val linearLayoutManager = LinearLayoutManager(mContext)
         linearLayoutManager.orientation = RecyclerView.VERTICAL
         holder.binding!!.rvOrderService.layoutManager = linearLayoutManager
--- app/src/main/java/com/example/services/adapters/orders/OrderServicesListAdapter.kt
+++ app/src/main/java/com/example/services/adapters/orders/OrderServicesListAdapter.kt
@@ -11,23 +11,29 @@ import com.bumptech.glide.Glide
 import com.bumptech.glide.load.resource.bitmap.RoundedCorners
 import com.bumptech.glide.request.RequestOptions
 import com.example.services.R
+import com.example.services.constants.GlobalConstants
 import com.example.services.databinding.OrderServiceItemBinding
 import com.example.services.model.orders.OrdersListResponse
 import com.example.services.utils.BaseActivity
+import com.example.services.utils.Utils
 
 class OrderServicesListAdapter(
-        context: BaseActivity,
-        addressList: ArrayList<OrdersListResponse.Suborders>?,
-        var activity: Context
+    context: BaseActivity,
+    addressList: ArrayList<OrdersListResponse.Suborders>?,
+    addressList1: ArrayList<OrdersListResponse.Body>,
+    var activity: Context
 ) :
         RecyclerView.Adapter<OrderServicesListAdapter.ViewHolder>() {
     private val mContext: BaseActivity
     private var viewHolder: ViewHolder? = null
     private var addressList: ArrayList<OrdersListResponse.Suborders>?
+    private var addressList1: ArrayList<OrdersListResponse.Body>?
+    private var orderListActivity = activity
 
     init {
         this.mContext = context
         this.addressList = addressList
+        this.addressList1 = addressList1
     }
 
     @NonNull
@@ -43,6 +49,162 @@ class OrderServicesListAdapter(
 
     override fun onBindViewHolder(@NonNull holder: ViewHolder, position: Int) {
         viewHolder = holder
+
+        holder.binding!!.tvOrderOn.text = Utils(mContext).getDate(
+            "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'",
+            addressList1?.get(position)!!.createdAt,
+            "HH:mm yyyy-MM-dd"
+        )
+        holder.binding!!.tvServiceOn.text = Utils(mContext).getDate(
+            "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'",
+            addressList1?.get(position)!!.serviceDateTime,
+            "HH:mm yyyy-MM-dd"
+        )
+
+        /*if (orderListActivity != null && addressList[position].trackStatus!!.equals("1")) {
+            holder.binding!!.tvTrack.visibility = View.VISIBLE
+        } else {
+            holder.binding!!.tvTrack.visibility = View.GONE
+        }*/
+        holder.binding!!.tvTotal.setText(GlobalConstants.Currency + " " + addressList1!![position].totalOrderPrice)
+////0-Pending/Not Confirmed, 1-> Confirmed , 2->Cancelled , 3->Processing,4//cancelled by company, 5->Completed
+        if (addressList1?.get(position)!!.cancellable.equals("true")) {
+            holder.binding!!.tvCancel.setText("Cancel Order"/*addressList[position].progressStatus*/)
+            holder.binding!!.tvCancel.isEnabled = true
+        } else {
+
+            if (addressList1?.get(position)!!.progressStatus.equals("0")) {
+                holder.binding!!.tvCancel.setText("Pending"/*addressList[position].progressStatus*/)
+                holder.binding!!.tvCancel.isEnabled = true
+                holder.binding!!.tvCancel.setBackgroundTintList(
+                    mContext.getResources().getColorStateList(
+                        R.color.colorStatus
+                    )
+                )
+
+            } else if (addressList1?.get(position)!!.progressStatus.equals("1")) {
+                holder.binding!!.tvCancel.setText("Confirmed"/*addressList[position].progressStatus*/)
+                holder.binding!!.tvCancel.isEnabled = true
+                holder.binding!!.tvCancel.setBackgroundTintList(
+                    mContext.getResources().getColorStateList(
+                        R.color.colorStatus
+                    )
+                )
+
+            } else if (addressList1?.get(position)!!.progressStatus.equals("2")) {
+                holder.binding!!.tvCancel.setText("Cancelled"/*addressList[position].progressStatus*/)
+                holder.binding!!.tvCancel.isEnabled = false
+            } else if (addressList1?.get(position)!!.progressStatus.equals("3")) {
+                holder.binding!!.tvCancel.isEnabled = true
+                holder.binding!!.tvCancel.setBackgroundTintList(
+                    mContext.getResources().getColorStateList(
+                        R.color.colorStatus
+                    )
+                )
+
+                holder.binding!!.tvCancel.setText("Processing"/*addressList[position].progressStatus*/)
+            } else if (addressList1?.get(position)!!.progressStatus.equals("4")) {
+                holder.binding!!.tvCancel.isEnabled = false
+                holder.binding!!.tvCancel.setText("Cancelled by company"/*addressList[position].progressStatus*/)
+            } else if (addressList1?.get(position)!!.progressStatus.equals("5")) {
+                holder.binding!!.tvCancel.isEnabled = false
+                holder.binding!!.tvCancel.setText("Completed"/*addressList[position].progressStatus*/)
+                holder.binding!!.tvCancel.setBackgroundTintList(
+                    mContext.getResources().getColorStateList(
+                        R.color.colorSuccess
+                    )
+                )
+
+            }
+        }
+
+        /*holder.binding!!.tvCancel.setOnClickListener {
+            if (addressList1?.get(position)!!.cancellable.equals("true")) {
+                if (orderListActivity != null)
+                    orderListActivity.cancelOrder(position)
+            } else {
+                orderListActivity!!.completeOrder(position)
+            }
+        }*/
+
+        holder.binding!!.tvOrderOn.text = Utils(mContext).getDate(
+            "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'",
+            addressList1?.get(position)?.createdAt,
+            "HH:mm yyyy-MM-dd"
+        )
+        holder.binding!!.tvServiceOn.text = Utils(mContext).getDate(
+            "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'",
+            addressList1?.get(position)?.serviceDateTime,
+            "HH:mm yyyy-MM-dd"
+        )
+
+        /*if (orderListActivity != null && addressList[position].trackStatus!!.equals("1")) {
+            holder.binding!!.tvTrack.visibility = View.VISIBLE
+        } else {
+            holder.binding!!.tvTrack.visibility = View.GONE
+        }*/
+        holder.binding!!.tvTotal.setText(GlobalConstants.Currency + " " + addressList1?.get(position)!!.totalOrderPrice)
+////0-Pending/Not Confirmed, 1-> Confirmed , 2->Cancelled , 3->Processing,4//cancelled by company, 5->Completed
+        if (addressList1?.get(position)?.cancellable.equals("true")) {
+            holder.binding!!.tvCancel.setText("Cancel Order"/*addressList[position].progressStatus*/)
+            holder.binding!!.tvCancel.isEnabled = true
+        } else {
+
+            if (addressList1!![position].progressStatus.equals("0")) {
+                holder.binding!!.tvCancel.setText("Pending"/*addressList[position].progressStatus*/)
+                holder.binding!!.tvCancel.isEnabled = true
+                holder.binding!!.tvCancel.setBackgroundTintList(
+                    mContext.getResources().getColorStateList(
+                        R.color.colorStatus
+                    )
+                )
+
+            } else if (addressList1!![position].progressStatus.equals("1")) {
+                holder.binding!!.tvCancel.setText("Confirmed"/*addressList[position].progressStatus*/)
+                holder.binding!!.tvCancel.isEnabled = true
+                holder.binding!!.tvCancel.setBackgroundTintList(
+                    mContext.getResources().getColorStateList(
+                        R.color.colorStatus
+                    )
+                )
+
+            } else if (addressList1!![position].progressStatus.equals("2")) {
+                holder.binding!!.tvCancel.setText("Cancelled"/*addressList[position].progressStatus*/)
+                holder.binding!!.tvCancel.isEnabled = false
+            } else if (addressList1!![position].progressStatus.equals("3")) {
+                holder.binding!!.tvCancel.isEnabled = true
+                holder.binding!!.tvCancel.setBackgroundTintList(
+                    mContext.getResources().getColorStateList(
+                        R.color.colorStatus
+                    )
+                )
+
+                holder.binding!!.tvCancel.setText("Processing"/*addressList[position].progressStatus*/)
+            } else if (addressList1!![position].progressStatus.equals("4")) {
+                holder.binding!!.tvCancel.isEnabled = false
+                holder.binding!!.tvCancel.setText("Cancelled by company"/*addressList[position].progressStatus*/)
+            } else if (addressList1!![position].progressStatus.equals("5")) {
+                holder.binding!!.tvCancel.isEnabled = false
+                holder.binding!!.tvCancel.setText("Completed"/*addressList[position].progressStatus*/)
+                holder.binding!!.tvCancel.setBackgroundTintList(
+                    mContext.getResources().getColorStateList(
+                        R.color.colorSuccess
+                    )
+                )
+
+            }
+        }
+
+        /*holder.binding!!.tvCancel.setOnClickListener {
+            if (addressList1!![position].cancellable.equals("true")) {
+                if (orderListActivity != null)
+                    orderListActivity.cancelOrder(position)
+            } else {
+                orderListActivity!!.completeOrder(position)
+            }
+        }*/
+
+
         holder.binding!!.tvServiceName.text = addressList!![position].service?.name
         holder.binding!!.tvQuantity.setText(mContext.resources.getString(R.string.quantity) + ": " + addressList!![position].quantity)
         // holder.binding!!.tvTime.setText(": " + addressList!![position].Timing)
